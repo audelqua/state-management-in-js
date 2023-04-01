d761c4afd4be15d1368870b98ddf817574f4dfbc
audrey <mbozorgi604@gmail.com>
2023-04-01 22:23:16 +0330

    add a function inside variable to add HTML tags into real DOM

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const name = 12
 
-console.log(name);
+
+//***** introducing a variable as a function which returns some HTML tags
+const App = function _App() {
+    return `
+        <h1>Hello Vanilla JS</h1>
+        <div>Example of state management in Vanilla JS</div>
+  `;
+}
+
+document.getElementById('root').innerHTML = App()
