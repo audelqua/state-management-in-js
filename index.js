d76aa4ae220459cf9680f325fffc16a03c7c7673
audrey <mbozorgi604@gmail.com>
2023-04-01 22:42:03 +0330

    functions of state management written without optimisation

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 
 
 //***** introducing a variable as a function which returns some HTML tags
-const App = function _App() {
-    return `
-        <h1>Hello Vanilla JS</h1>
-        <div>Example of state management in Vanilla JS</div>
-  `;
+const App = {
+    //***** add state management including variable as state and a function which updates it.
+    state: {
+        count: 0,
+        increment: function() {
+            App.state.count += 1
+            someFunction() // ****** call document and its update to make the real DOM reflect state changes. 
+        }
+    },
+    //****** add a property which returns HTML elements to make state visible and a button to change it
+    _App: () => `
+            <h1>${App.state.count}</h1>
+            <button id='button'>Increase the number</button>
+        `
 }
 
-document.getElementById('root').innerHTML = App()
+
+//***** add a function which re-generate after increment function called.
+function someFunction () {
+    document.getElementById('root').innerHTML = App._App()
+    document.getElementById('button').addEventListener('click', App.state.increment)
+}
+someFunction()
+
